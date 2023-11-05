eb5439b6bebe410ba896fbb15b6f06de93b8b69e
farukwebcode21 <farukahmadmk@yahoo.com>
2023-11-05 15:38:33 +0600

    npm xss clearn

--- server/README.md
+++ server/README.md
@@ -8,5 +8,7 @@
  4. HTTP request & response
  5. nodemon & morgan package
  6. Api testing with Postman & Thunder client
- 7. Middleware
+ 7. Middleware & Types of Middleware
+ 8. Express Error Handling Middleware
+ 9. How to handle HTTP errors ->http-errors
  
--- server/src/server.js
+++ server/src/server.js
@@ -1,46 +1,4 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const isLoggedIn = (req, res, next) => {
-  const login = true;
-  if (login) {
-    req.body.id = 101;
-    next();
-  } else {
-    return res.status(401).json({ message: "please login first" });
-  }
-};
-
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: "Welcome to the server server locally running",
-  });
-});
-
-app.get("/api/user", isLoggedIn, (req, res) => {
-  console.log(req.body.id);
-  res.status(200).send({
-    message: "user profile is return",
-  });
-});
-
-// client error handling
-app.use((req, res, next) => {
-  res.status(404).json({ message: "route not found" });
-  next();
-});
-
-// server error handling
-app.use((err, req, res, next) => {
-  console.log(err.stack);
-  res.status(500).send("something broke !");
-});
+const app = require("./app");
 
 app.listen(3001, () => {
   console.log(`server is running at http://localhost:3001`);
