5d0ae70233d763bceea48d5d5b67b08e3482e699
farukwebcode21 <farukahmadmk@yahoo.com>
2023-11-05 20:19:16 +0600

    add gitignore

--- server/README.md
+++ server/README.md
@@ -6,5 +6,9 @@
  2. Environment setup
  3. Create express server
  4. HTTP request & response
-
+ 5. nodemon & morgan package
+ 6. Api testing with Postman & Thunder client
+ 7. Middleware & Types of Middleware
+ 8. Express Error Handling Middleware
+ 9. How to handle HTTP errors ->http-errors
  
---
+++ server/src/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const app = express();
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 100,
+  max: 5,
+  message: "Too many request this IP. please try again later",
+});
+
+app.use(rateLimiter);
+app.use(xssClean());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// understand Middleware working system
+
+// const isLoggedIn = (req, res, next) => {
+//   const login = true;
+//   if (login) {
+//     req.body.id = 101;
+//     next();
+//   } else {
+//     return res.status(401).json({ message: "please login first" });
+//   }
+// };
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the server server locally running",
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to test route",
+  });
+});
+
+app.get("/api/user", (req, res) => {
+  console.log(req.body.id);
+  res.status(200).send({
+    message: "user profile is return",
+  });
+});
+
+// client error handling
+app.use((req, res, next) => {
+  next(createError(404, "route not found"));
+});
+
+// server error handling
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+module.exports = app;
